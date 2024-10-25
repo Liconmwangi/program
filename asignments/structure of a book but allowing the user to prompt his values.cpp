f23201c28f00741d592acedec335f5b38540c18b
Liconmwangi <lincolnmwangi56@gmail.com>
2024-10-25 00:15:51 -0700

    Create structure of a book but allowing the user to prompt his values.cpp

---
+++ asignments/structure of a book but allowing the user to prompt his values.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <conio.h>
+#include<string.h>
+
+	struct book
+	{
+	char title[30];
+	char author[30];
+	int publication_year;
+	char ISBN[13];
+	float price;
+
+}
+book,book1;
+int main()
+{
+	printf("Enter the title of the book\n");
+	scanf("%s",book1.title);
+	printf("Enter the book author\n");
+	scanf("%s",book1.author);
+	printf("Enter the book publication year\n");
+	scanf("%d",book1.publication_year);
+	printf("Enter the price of the book\n");
+	scanf("%d",book1.price);
+	printf("Ente the ISBN of the book\n");
+	scanf("%s",book1.ISBN of the book\n);
+	
+	
+getch();	
+}
